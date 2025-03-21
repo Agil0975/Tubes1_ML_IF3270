d122ab7931c64a81f24c3235b7935068897f5427
Agil0975 <agilfadillahsabri03@gmail.com>
2025-03-21 09:03:34 +0700

    feat: add Activation Function

--- FFNN/ActivationFunction.py
+++ FFNN/ActivationFunction.py
@@ -126,3 +126,16 @@ class ActivationFunction:
         """
         e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
         return e_x / np.sum(e_x, axis=1, keepdims=True)
+    
+    def softmax_derivative(self, x: np.ndarray) -> np.ndarray:
+        """
+        Derivative of the softmax activation function.
+
+        Parameters:
+        x (np.ndarray): Input array.
+
+        Returns:
+        np.ndarray: Derivative of the softmax activation function.
+        """
+        s = self.softmax(x)
+        return s * (1 - s)
