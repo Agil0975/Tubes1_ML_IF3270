0f4a9effba0d91dce6580b296285ea4cc3816f95
Agil0975 <agilfadillahsabri03@gmail.com>
2025-03-21 15:02:01 +0700

    feat: add some methods in FFNN (implement later)

--- FFNN/FFNN.py
+++ FFNN/FFNN.py
@@ -17,9 +17,10 @@ class FFNN:
         self._lf = lf.LossFunction()        # Initialize the loss function object
         self._af = af.ActivationFunction()  # Initialize the activation function object
 
-    def add_layer(self, *, 
-                  num_neurons, activation_function='sigmoid', 
-                  initialization_method='normal', seed=None,
+    def add_layer(self, num_neurons, *,
+                  activation_function='sigmoid', 
+                  initialization_method='normal', 
+                  seed=None,
                   lower_bound=0, upper_bound=1,  # for uniform initialization
                   mean=0, std=1,                 # for normal initialization
                  ):
@@ -101,7 +102,111 @@ class FFNN:
         else:
             raise ValueError(f"Unknown initialization method: {method}. Use 'zero', 'uniform', or 'normal'.")
         
+    def __feed_forward(self, x: np.ndarray) -> np.ndarray:
+        """
+        Perform feedforward operation.
+
+        Parameters:
+        x (np.ndarray): Input data.
+
+        Returns:
+        np.ndarray: Output of the neural network.
+        """
+        for i in range(1, len(self.layers)):
+            x = self._af.activation(x @ self.weights[i-1] + self.biases[i-1],
+                                  activation_function=self.activations[i-1])
+        return x
+
+    def __back_propagation(self, x: np.ndarray, y: np.ndarray) -> None:
+        """
+        Perform backpropagation to update weights and biases.
+
+        Parameters:
+        x (np.ndarray): Input data.
+        y (np.ndarray): True labels.
+        """
+        pass
+
+    def train(self, x: np.ndarray, y: np.ndarray, *, 
+              batch_size: int,
+              learning_rate: float,
+              epochs: int,
+              loss_function: str,
+              verbose: int = 1,
+              validation_split: float = 0.2,
+              ):
+        """
+        Train the neural network.
+        
+        Parameters:
+        x (np.ndarray): Input data.
+        y (np.ndarray): True labels.
+        batch_size (int): Size of each batch for training.
+        learning_rate (float): Learning rate for the optimizer.
+        epochs (int): Number of epochs for training.
+        loss_function (str): Loss function to be used for training.
+        verbose (int): Verbosity mode. 0 = silent, 1 = progress bar + training info
+
+        returns:
+        np.ndarray: history of the loss function (training and validation loss) for each epoch
+        """
+        pass
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Make predictions using the trained model.
+
+        Parameters:
+        x (np.ndarray): Input data.
+
+        Returns:
+        np.ndarray: Predicted labels.
+        """
+        return self.__feed_forward(x)
     
+    def plot_network_graph(self):
+        """
+        Plot the network graph.
+        """
+        pass
+
+    def plot_weight_distribution(layers: list[int]):
+        """
+        Plotting the weight distribution for a specific layer
+
+        Parameters:
+        layers (list[int]): List of layers to be plotted
+        """
+        pass
+
+    def plot_gradient_distribution(layers: list[int]):
+        """
+        Plotting the gradient weight distribution for a specific layer
+
+        Parameters:
+        layers (list[int]): List of layers to be plotted
+        """
+        pass
+
+    def save(self, filename: str):
+        """
+        Save the model network to a file.
+
+        Parameters:
+        filename (str): Name of the file to save the neural network.
+        """
+        pass
+
+    def load(self, filename: str):
+        """
+        Load the model network from a file.
+
+        Parameters:
+        filename (str): Name of the file to load the neural network.
+        """
+        pass
+
+
 
 # a = 5
 # b = 10
@@ -115,9 +220,20 @@ class FFNN:
 # print(f"Zero initialization: {np.zeros((a, b))}")
 # print(f"Zero initialization: {np.zeros(a)}")
 
-a = np.array([[1, 2, 3], 
-              [4, 5, 6]])
-b = np.array([[7, 8, 9],
-              [10, 11, 12],
-              [13, 14, 15]])
-print(np.dot(a, b))  # Transpose b to match the dimensions for dot product
+# a = np.array([[1, 2, 3], 
+#               [4, 5, 6]])
+# b = np.array([[7, 8, 9],
+#               [10, 11, 12],
+#               [13, 14, 15]])
+# print(np.dot(a, b))  # Transpose b to match the dimensions for dot product
+# print(a @ b)  # Using the @ operator for matrix multiplication
+# print(np.matmul(a, b))  # Using np.matmul for matrix multiplication
+
+# a = np.array([[1, 2], 
+#               [3, 4],
+#               [5, 6]])
+# b = np.array([100, 1000])
+
+# print(a)
+# print(b)
+# print(a+b)  # Element-wise addition
